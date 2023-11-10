26ef6abe5ae9b7f0cdf7b8f6a4a56dde878e7120
subhammahanty235 <subhammahanty235@gmail.com>
2023-11-10 23:52:24 +0530

    fixed create profile

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,15 +5,17 @@ exports.createProfile = async(req,res) => {
         const {profileData , userId} = req.body
 
         //check if profile exists or not
-        const user = await User.findOne({id:userId});
+        const user = await User.findOne({_id:userId});
+       
         if(!user){
             return res.status(400).json({success:false , message:"User not exists"});
         }
 
         const response = await User.findByIdAndUpdate(userId , profileData);
+        
         if(response){
-           const response =  await User.findByIdAndUpdate(response.id , {profileCreated:true , joined:new Date()})
-            if(response){
+           const responsein =  await User.findByIdAndUpdate(response.id , {profileCreated:true , joined:new Date()})
+            if(responsein){
                 return res.status(200).json({success:true , message:"Profie created successfully"})
             }
         }else{
