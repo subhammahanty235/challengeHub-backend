7c231ed288cbb0742852fe76bbf8a1982b6d0812
subhammahanty235 <subhammahanty235@gmail.com>
2023-11-17 22:52:04 +0530

    mark day as complted controller and api created

--- routes/challenge.route.js
+++ routes/challenge.route.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
-const {findAllChallenges , findChallengesofUser , createChallenge} = require('../controllers/challengeController')
+const {findAllChallenges , findChallengesofUser , createChallenge, marktaskasDone} = require('../controllers/challengeController')
 
 router.get('/getAll/:userId' ,findAllChallenges);
 router.get('/getmyChallenges/:userId' , findChallengesofUser)
 router.post('/create' ,createChallenge )
-
+router.post('/markasdone/:cId/:userId' , marktaskasDone)
 module.exports = router
